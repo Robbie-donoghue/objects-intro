5520dba2eed4ff618da57c1a8adc8d36436691d0
Robbie <Leon.Wh1te@proton.me>
2024-01-16 11:02:19 +0000

    Basic Objects

---
+++ app.js
@@ -0,0 +1,26 @@
+let person = 
+{
+    fName : "Robbie" ,
+    age : 19 ,
+    favouriteColour : "red"
+
+}
+console.log(person)
+
+console.table(person)
+
+console.log(person.age)
+// add new property
+person.favourateFood = "pizza" , "french fry"
+
+console.log(person.favouriteFood) 
+// pizza , french fry
+
+let car = 
+{
+    make: `Toyota` , 
+    model: `Yaris` , 
+    colour: `Grey`
+}
+// log it all to console for Advertising 
+console.log( " this is a " ,car.make , "It is  a "  ,car.make , car.model , "and it's available in" , car.colour) 
